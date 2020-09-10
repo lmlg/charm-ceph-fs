650c2d6096847f9cf890c5ce9483a7bb44e5b8f5
Liam Young <liam.young@canonical.com>
2020-09-10 14:49:10 +0000

    Add EC fix and tests
    When using erasure coding three pools are needed. One metadata
    pool which is replicated, one default data pool which is replicated
    and one erasure coded pool.
    
    The consumer of the share will need to use xattrs to ensure that
    the hierarchy of directories and files are stored in EC pool while
    leaving the default replicated data pool for file backtraces.
    
    Depends-On: Ib19e545fc676fa9e986f3fddcfb92cf99e778a3f
    Depends-On: I48348ab2b6c8952c5e22008a074a60c1f35be952
    Change-Id: I1af386d5e1d031f2f0387c7381af683f948172d9
    Closes-Bug: #1895154

--- src/reactive/ceph_fs.py
+++ src/reactive/ceph_fs.py
@@ -76,6 +76,7 @@ def storage_ceph_connected(ceph):
     metadata_weight = weight * 0.20
     # Resize data pool weight to accomodate metadata weight
     weight = weight - metadata_weight
+    extra_pools = []
 
     if config('pool-type') == 'erasure-coded':
         # General EC plugin config
@@ -111,18 +112,25 @@ def storage_ceph_connected(ceph):
         )
 
         # Create EC data pool
+        ec_pool_name = 'ec_{}'.format(pool_name)
         ceph_mds.create_erasure_pool(
-            name=pool_name,
+            name=ec_pool_name,
             erasure_profile=profile_name,
             weight=weight,
             app_name=ceph_mds.ceph_pool_app_name,
             allow_ec_overwrites=True
         )
         ceph_mds.create_replicated_pool(
+            name=pool_name,
+            weight=metadata_weight,
+            app_name=ceph_mds.ceph_pool_app_name
+        )
+        ceph_mds.create_replicated_pool(
             name=metadata_pool_name,
             weight=metadata_weight,
             app_name=ceph_mds.ceph_pool_app_name
         )
+        extra_pools = [ec_pool_name]
     else:
         ceph_mds.create_replicated_pool(
             name=pool_name,
@@ -134,5 +142,5 @@ def storage_ceph_connected(ceph):
             replicas=replicas,
             weight=metadata_weight,
             app_name=ceph_mds.ceph_pool_app_name)
-    ceph_mds.request_cephfs(service)
+    ceph_mds.request_cephfs(service, extra_pools=extra_pools)
     reactive.set_state('ceph.create_pool.req.sent')
